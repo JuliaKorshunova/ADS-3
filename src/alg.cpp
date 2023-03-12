9c7ce427bd21566aa65f307dfa2fedd9c8734cc5
JuliaKorshunova <116427627+JuliaKorshunova@users.noreply.github.com>
2023-03-12 18:38:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,28 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int left = 0;
+    int right = size - 1;
+    int kolv = 0;
+    while (right >= left) {
+        int middle = (left + right) / 2;
+        if (arr[middle] == value) {
+            kolv += 1;
+            int newmiddel = middel;
+            while (arr[middle + 1]) == value) {
+                kolv += 1;
+                middle += 1;
+            }
+            while (arr[newmiddle - 1]) == value) {
+                kolv += 1;
+                newmiddle -= 1;
+            }
+            return kolv;
+        } else if (arr[middle]) > value) {
+            left = middle + 1;
+        } else {
+            right = middle - 1;
+        }
+    }
+    return kolv;
 }
